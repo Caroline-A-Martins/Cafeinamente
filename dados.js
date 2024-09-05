f2ab968ccf066100d3cff692484da3da3bf0e368
Caroline-A-Martins <business.carolmartins@gmail.com>
2024-09-05 19:59:17 -0300

    Aula 2 e 3

---
+++ dados.js
@@ -0,0 +1,110 @@
+let dados = [
+    {
+        titulo: "Capuccino",
+        descricao:
+        "Uma bebida clássica e apreciada em todo o mundo, o capuccino é a combinação perfeita entre café espresso, leite vaporizado e espuma cremosa.",
+        ingredientes: ["café espresso", "leite integral", "canela em pó"],
+    },
+    {
+        titulo: "Tiramisu",
+        descricao:
+        "O Tiramisu é uma sobremesa italiana que conquistou paladares ao redor do mundo. Com camadas alternadas de biscoitos macios embebidos em café forte e um creme leve e cremoso feito com mascarpone, ovos e açúcar, essa sobremesa oferece uma combinação perfeita de sabores e texturas.",
+        ingredientes: [
+        "Biscoitos Ladyfingers",
+        "Café Forte",
+        "Mascarpone",
+        "Ovos",
+        "Açúcar",
+        "Cacau em Pó",
+        "Licor (opcional)",
+        ],
+    },
+    {
+        titulo: "Frapuccino",
+        descricao:
+        "O Frappuccino é uma bebida gelada e cremosa que conquistou o paladar de milhões de pessoas ao redor do mundo. Essa mistura deliciosa de café, leite, gelo e diversos sabores é perfeita para se refrescar nos dias quentes ou para quem busca uma opção energizante.",
+        ingredientes: [
+        "Café",
+        "Leite",
+        "Gelo",
+        "Açúcar",
+        "Saborizantes (opcional)",
+        ],
+    },
+    {
+        titulo: "Mocha",
+        descricao:
+        "O mocha é uma bebida quente que combina café expresso, chocolate e leite vaporizado, criando uma experiência saborosa e reconfortante.",
+        ingredientes: [
+        "Café Expresso",
+        "Chocolate",
+        "Leite Vaporizado",
+        "Açúcar (opcional)",
+        ],
+    },
+    {
+        titulo: "Latte",
+        descricao:
+        "O latte é uma bebida à base de espresso e leite vaporizado, conhecido por sua textura suave e cremosa.",
+        ingredientes: ["Café Expresso", "Leite Vaporizado"],
+    },
+    {
+        titulo: "Macchiato",
+        descricao:
+        "O macchiato é um espresso com um toque de leite, perfeito para quem gosta de um café mais forte com um toque cremoso.",
+        ingredientes: ["Café Expresso", "Leite (opcional)"],
+    },
+    {
+        titulo: "Affogato",
+        descricao:
+        "O affogato é uma sobremesa simples e deliciosa que consiste em uma bola de sorvete coberta com espresso quente.",
+        ingredientes: ["Sorvete", "Café Expresso"],
+    },
+    {
+        titulo: "Irish Coffee",
+        descricao:
+        "O Irish Coffee é uma bebida quente e alcoólica que combina café, uísque irlandês, açúcar e chantilly.",
+        ingredientes: ["Café", "Uísque Irlandês", "Açúcar", "Chantilly"],
+    },
+    {
+        titulo: "Café Gelado",
+        descricao:
+        "O café gelado é uma opção refrescante feita com café preparado e resfriado, servido com gelo e, frequentemente, adoçado com leite ou creme.",
+        ingredientes: [
+        "Café",
+        "Gelo",
+        "Leite ou Creme (opcional)",
+        "Açúcar (opcional)",
+        ],
+    },
+    {
+        titulo: "Café au Lait",
+        descricao:
+        "O café au lait é uma bebida francesa que combina café filtrado com uma quantidade igual de leite vaporizado, criando um equilíbrio perfeito entre café e leite.",
+        ingredientes: ["Café Filtrado", "Leite Vaporizado"],
+    },
+    {
+        titulo: "Affogato al Caffè",
+        descricao:
+        "Uma variação do affogato, o affogato al caffè é uma sobremesa onde uma bola de sorvete é coberta com um espresso de café forte e fresco.",
+        ingredientes: ["Sorvete", "Café Expresso"],
+    },
+    {
+        titulo: "Café Cortado",
+        descricao:
+        "O café cortado é uma bebida espanhola que combina uma dose de café expresso com uma quantidade igual de leite quente, resultando em uma bebida rica e equilibrada.",
+        ingredientes: ["Café Expresso", "Leite Quente"],
+    },
+    {
+        titulo: "Café Preto",
+        descricao:
+        "O café preto, na sua forma mais pura, é a infusão dos grãos de café moídos em água quente. Sem adição de leite, açúcar ou outros ingredientes, ele revela o sabor mais autêntico do café, com suas nuances de amargor, acidez e corpo.",
+        ingredientes: ["Café Moído", "Água Quente"],
+    },
+    {
+        titulo: "Espresso",
+        descricao:
+        "O espresso é um café forte e concentrado, preparado por meio da passagem de água quente sob alta pressão através de grãos de café moídos finamente.",
+        ingredientes: ["Café Moído", "Água Quente"],
+    },
+];
